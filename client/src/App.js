d882d348d58f5bfae9cd6c161bb22a8aab63a0d5
Alice Chang <sinsofangels@gmail.com>
2021-02-17 22:21:30 -0500

    add button with function to call localhost with token

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
 function App() {
+  function callBE() {
+    let token = ""
+    axios.post("http://localhost:5000/callDS", token)
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <button onClick={callBE}>Sign your life away</button> 
     </div>
   );
 }
